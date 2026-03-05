7aea5dcbf9fec2d71671f6b787731479870e79e7
Akshay Narayan <akshayn@brown.edu>
2026-03-05 13:20:07 -0500

    Generate bib file

--- Makefile
+++ Makefile
@@ -1,16 +1,19 @@
 all: index.html coffee.html res/akshay-narayan-cv.pdf
 
 coffee.html: coffee/*.rkt main.css map.css
-	racket coffee/index.rkt > coffee.html || rm coffee.html
+	racket coffee/index.rkt > $@ || rm $@
 
 index.html: index.rkt publications.rkt service.rkt main.css
-	racket index.rkt > index.html || rm index.html
+	racket $< > $@ || rm $@
+
+akshay.bib: bib.rkt publications.rkt
+	racket $< $@
 
 cv.tex: cv.rkt cv/*.tex publications.rkt service.rkt students.rkt
-	raco scribble --latex cv.rkt
+	raco scribble --latex $<
 
 cv.pdf: cv.tex
-	tectonic cv.tex
+	tectonic $<
 
 res/akshay-narayan-cv.pdf: cv.pdf
-	cp cv.pdf res/akshay-narayan-cv.pdf
+	cp $< $@
---
+++ bib.rkt
@@ -0,0 +1,23 @@
+#lang racket/base
+
+(require
+  racket/string
+  racket/list
+  (except-in "publications.rkt" doc)
+)
+
+(define (bibtex publication)
+  (string-join (list
+    "@inproceedings{" (publication-key publication) ",\n"
+    "title = {{" (publication-title publication) "}},\n"
+    "author = {" (string-join (publication-authors-str publication) " and ") "},\n"
+    "year = {" (number->string (publication-year publication)) "},\n"
+    "booktitle = {" (publication-venue publication) "},\n"
+    "doi = {" (publication-doi publication) "},\n"
+    "}\n"
+  ))
+)
+
+(define bibfile (open-output-file (vector-ref (current-command-line-arguments) 0) #:exists 'truncate))
+(display (for/list ([p (publications)]) (bibtex p)) bibfile)
+(close-output-port bibfile)
